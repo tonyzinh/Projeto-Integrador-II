1e44c8a166853768813289493d8c43e2ab2f5bbe
Tony <entonysantos@outlook.com.br>
2024-10-14 17:56:06 -0300

    adição de páginas no site

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,1 +1,29 @@
-ECHO est  ativado.
+document.addEventListener('DOMContentLoaded', () => {
+    const pageUrls = {
+        home: './index.html',
+        sobreNos: './pages/error.html',
+        psicologos: './pages/error.html',
+        empresas: './pages/error.html',
+        suporte: './pages/error.html'
+    };
+
+    document.querySelector('.home1').addEventListener('click', () => {
+        window.location.href = pageUrls.home;
+    });
+
+    document.querySelector('.sobre-ns').addEventListener('click', () => {
+        window.location.href = pageUrls.sobreNos;
+    });
+
+    document.querySelector('.psiclogos').addEventListener('click', () => {
+        window.location.href = pageUrls.psicologos;
+    });
+
+    document.querySelector('.empresas').addEventListener('click', () => {
+        window.location.href = pageUrls.empresas;
+    });
+
+    document.querySelector('.suporte').addEventListener('click', () => {
+        window.location.href = pageUrls.suporte;
+    });
+});
